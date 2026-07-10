16a877df5642599403d2d840c1fb9bd63aaf0f14
Florian Trück <ftrueck@gmail.com>
2026-07-10 14:22:13 +0200

    Expose BLE diagnostics API filters

--- api.js
+++ api.js
@@ -2,6 +2,14 @@
 
 module.exports = {
   async scanBle({ homey }) {
-    return homey.app.logBleScan();
+    return homey.app.logBleScan('all');
+  },
+
+  async scanMatchedBle({ homey }) {
+    return homey.app.logBleScan('matched');
+  },
+
+  async scanUnknownBle({ homey }) {
+    return homey.app.logBleScan('unknown');
   },
 };
